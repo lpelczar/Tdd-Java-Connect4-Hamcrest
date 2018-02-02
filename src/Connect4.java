a75566236ea33d84f91dbe79eae5004190348d99
lpelczar <lukepelczar@gmail.com>
2018-02-02 19:07:37 +0100

    Implement putting discs

--- src/Connect4.java
+++ src/Connect4.java
@@ -26,4 +26,26 @@ public class Connect4 {
             Arrays.fill(column, Color.EMPTY);
         }
     }
+
+    public void putDisc(int column) {
+        if (column > 0 && column <= COLUMNS) {
+            int numOfDiscs = getNumberOfDiscsInColumn(column - 1);
+            if (numOfDiscs < ROWS) {
+                board[column - 1][numOfDiscs] = Color.RED;
+            }
+        }
+    }
+
+    public int getNumberOfDiscsInColumn(int column) {
+        if (column >= 0 && column < COLUMNS) {
+            int row;
+            for (row = 0; row < ROWS; row++) {
+                if (Color.EMPTY == board[column][row]) {
+                    return row;
+                }
+            }
+            return row;
+        }
+        return -1;
+    }
 }
