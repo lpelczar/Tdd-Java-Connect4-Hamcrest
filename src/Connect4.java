27998d9d43d08a1d88c94aef0825766859d6ffc0
lpelczar <lukepelczar@gmail.com>
2018-02-02 18:57:42 +0100

    Implement initializing board with empty colors

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Connect4 {
 
     public enum Color {
@@ -12,6 +14,16 @@ public class Connect4 {
         @Override
         public String toString() {
             return String.valueOf(value);
+        }
+    }
+
+    public static final int COLUMNS = 7;
+    public static final int ROWS = 6;
+    private Color[][] board = new Color[COLUMNS][ROWS];
+
+    public Connect4() {
+        for (Color[] column: board) {
+            Arrays.fill(column, Color.EMPTY);
         }
     }
 }
