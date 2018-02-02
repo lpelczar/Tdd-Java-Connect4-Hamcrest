596148bcedf8d2a1709899b96e4470de828738c1
lpelczar <lukepelczar@gmail.com>
2018-02-02 18:55:02 +0100

    Add enum for colors

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,2 +1,17 @@
 public class Connect4 {
+
+    public enum Color {
+        RED('R'), GREEN('G'), EMPTY('E');
+
+        private final char value;
+
+        Color(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
 }
