fd08ad318e601aea5c16d248608e7313109e1dae
lpelczar <lukepelczar@gmail.com>
2018-02-14 13:14:07 +0100

    Add passing test checking if column is outside board

--- src/main/java/Connect4TDD.java
+++ src/main/java/Connect4TDD.java
@@ -3,4 +3,10 @@ public class Connect4TDD {
     public int getNumberOfDiscs() {
         return 0;
     }
+
+    public void putDiscInColumn(int column) {
+        if (column < 0 || column > 6) {
+            throw new RuntimeException("Wrong column " + column);
+        }
+    }
 }
