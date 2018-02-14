ea5311c966644a97e598089cc80fffdb05c4cf84
lpelczar <lukepelczar@gmail.com>
2018-02-14 13:19:54 +0100

    Implement passing test

--- src/main/java/Connect4TDD.java
+++ src/main/java/Connect4TDD.java
@@ -4,9 +4,11 @@ public class Connect4TDD {
         return 0;
     }
 
-    public void putDiscInColumn(int column) {
+    public int putDiscInColumn(int column) {
         if (column < 0 || column > 6) {
             throw new RuntimeException("Wrong column " + column);
+        } else {
+            return 0;
         }
     }
 }
