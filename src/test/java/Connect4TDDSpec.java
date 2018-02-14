dac620065149650cfaf0c6fad3399280819ced01
lpelczar <lukepelczar@gmail.com>
2018-02-14 12:52:32 +0100

    Add test checking if the board is empty when game started

--- src/test/java/Connect4TDDSpec.java
+++ src/test/java/Connect4TDDSpec.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+
 public class Connect4TDDSpec {
+
+    private Connect4TDD tested;
+
+    @Before
+    public void beforeEachTest() {
+        tested = new Connect4TDD();
+    }
+
+    @Test
+    public void whenTheGameIsStartedTheBoardIsEmpty() {
+        assertThat(tested.getNumberOfDiscs(), is(0));
+    }
 }
