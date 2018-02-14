8ccd6e4182754955906cd02526249da3a0c81be3
lpelczar <lukepelczar@gmail.com>
2018-02-14 13:41:58 +0100

    Implement passing test when second disc is inserted

--- src/main/java/Connect4TDD.java
+++ src/main/java/Connect4TDD.java
@@ -1,14 +1,40 @@
+import java.util.Arrays;
+
 public class Connect4TDD {
+
+    private static final int ROWS = 6;
+    private static final int COLUMNS = 7;
+    private static final String EMPTY = " ";
+    private String[][] board = new String[ROWS][COLUMNS];
+
+    Connect4TDD() {
+        for (String[] row : board) {
+            Arrays.fill(row, EMPTY);
+        }
+    }
 
     public int getNumberOfDiscs() {
         return 0;
     }
 
     public int putDiscInColumn(int column) {
+        int row;
         if (column < 0 || column > 6) {
             throw new RuntimeException("Wrong column " + column);
         } else {
-            return 0;
+            row = getNumberOfDiscsInColumn(column);
+            board[row][column] = "X";
         }
+        return row;
+    }
+
+    private int getNumberOfDiscsInColumn(int column) {
+        int numOfDiscs = 0;
+        for (String[] row : board) {
+            if (row[column].equals("X")) {
+                numOfDiscs++;
+            }
+        }
+        return numOfDiscs;
     }
 }
